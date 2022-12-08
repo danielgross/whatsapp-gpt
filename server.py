ddb42a7b6f63804f12ba361982851ae13658838e
DG <daniel@dcgross.com>
2022-12-08 14:25:06 -0800

    fix selector bug

--- server.py
+++ server.py
@@ -3,26 +3,36 @@
 import time
 import os 
 import flask
+import sys
 
 from flask import g
 
 from playwright.sync_api import sync_playwright
 
+PROFILE_DIR = "/tmp/playwright" if '--profile' not in sys.argv else sys.argv[sys.argv.index('--profile') + 1]
+PORT = 5001 if '--port' not in sys.argv else int(sys.argv[sys.argv.index('--port') + 1])
 APP = flask.Flask(__name__)
 PLAY = sync_playwright().start()
 BROWSER = PLAY.chromium.launch_persistent_context(
-    user_data_dir="/tmp/playwright",
+    user_data_dir=PROFILE_DIR,
     headless=False,
 )
 PAGE = BROWSER.new_page()
 
 def get_input_box():
-    """Get the child textarea of `PromptTextarea__TextareaWrapper`"""
-    return PAGE.query_selector("div[class*='PromptTextarea__TextareaWrapper']").query_selector("textarea")
+    """Find the input box by searching for the largest visible one."""
+    textareas = PAGE.query_selector_all("textarea")
+    candidate = None
+    for textarea in textareas:
+        if textarea.is_visible():
+            if candidate is None:
+                candidate = textarea
+            elif textarea.bounding_box().width > candidate.bounding_box().width:
+                candidate = textarea
+    return candidate
 
 def is_logged_in():
     try:
-        # See if we have a textarea with data-id="root"
         return get_input_box() is not None
     except AttributeError:
         return False
@@ -38,8 +48,8 @@ def send_message(message):
 
 def get_last_message():
     """Get the latest message"""
-    page_elements = PAGE.query_selector_all("div[class*='ConversationItem__Message']")
-    last_element = page_elements[-1]
+    page_elements = PAGE.query_selector_all(".flex.flex-col.items-center > div")
+    last_element = page_elements[-2]
     return last_element.inner_text()
 
 @APP.route("/chat", methods=["GET"])
@@ -53,13 +63,13 @@ def chat():
 
 def start_browser():
     PAGE.goto("https://chat.openai.com/")
+    APP.run(port=PORT, threaded=False)
     if not is_logged_in():
         print("Please log in to OpenAI Chat")
         print("Press enter when you're done")
         input()
     else:
         print("Logged in")
-        APP.run(port=5001, threaded=False)
         
 if __name__ == "__main__":
     start_browser()
