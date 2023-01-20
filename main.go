37077c209faf8ae7ba50a57175111b4e0eb3694a
Shushu Inbar <iinbar@tripactions.com>
2023-01-21 01:50:35 +0200

    cleanup

--- main.go
+++ main.go
@@ -1,47 +1,30 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/mdp/qrterminal/v3"
 	"github.com/snowflakedb/gosnowflake"
-	"go.mau.fi/whatsmeow"
-	waProto "go.mau.fi/whatsmeow/binary/proto"
-	"go.mau.fi/whatsmeow/store/sqlstore"
-	"go.mau.fi/whatsmeow/types"
-	"go.mau.fi/whatsmeow/types/events"
-	waLog "go.mau.fi/whatsmeow/util/log"
-	"google.golang.org/protobuf/proto"
 	"io"
 	"log"
-	"mime"
 	"net/http"
 	"net/url"
 	"os"
 	"os/exec"
-	"os/signal"
-	"regexp"
 	"strings"
-	"syscall"
 )
 
 func getConfig() gosnowflake.Config {
 	return gosnowflake.Config{
-		Account:   "XXXX",
-		User:      "XXX",
-		Password:  "XXX",
-		Database:  "XXX",
-		Warehouse: "XXX",
+		Account:   "iz80647",
+		User:      "SVC_OPENAI_CONNECT",
+		Password:  "rub!QYE!juz4nzn@rzy",
+		Database:  "staging",
+		Warehouse: "data_Team",
 	}
-}
-
-type MyClient struct {
-	WAClient       *whatsmeow.Client
-	eventHandlerID uint32
 }
 
 type ExistingSchema struct {
@@ -49,125 +32,86 @@ type ExistingSchema struct {
 	tables []string
 }
 
-func (mycli *MyClient) register() {
-	mycli.eventHandlerID = mycli.WAClient.AddEventHandler(mycli.eventHandler)
+func convertToWav(path string) string {
+	cmd := exec.Command("/opt/homebrew/bin/ffmpeg", "-y", "-i", path, path+".wav")
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return path + ".wav"
 }
 
-func (mycli *MyClient) eventHandler(evt interface{}) {
-	switch v := evt.(type) {
-	case *events.Message:
-		newMessage := v.Message
-		msg := newMessage.GetConversation()
-		fmt.Println("Message from:", v.Info.Sender.User, "->", msg)
-		if msg == "" && v.Message.DocumentMessage == nil && v.Message.AudioMessage == nil {
-			return
-		}
-		var response string
-		if v.Message.AudioMessage != nil {
-			audio := v.Message.GetAudioMessage()
-			data, err := mycli.WAClient.Download(audio)
-			if err != nil {
-				log.Printf("Failed to download file: %v", err)
-				//return response
-			}
-			exts, _ := mime.ExtensionsByType(audio.GetMimetype())
-			path := fmt.Sprintf("%s%s", "audio", exts[0])
-			err = os.WriteFile(path, data, 0600)
-			if err != nil {
-				log.Printf("Failed to save document: %v", err)
-				//return response
-			}
-			log.Printf("Saved document in message to %s", path)
-			// Use the ffmpeg command to convert the file
-			cmd := exec.Command("/opt/homebrew/bin/ffmpeg", "-y", "-i", path, path+".wav")
-			err = cmd.Run()
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
+/*
+	schema := getSchema()
+	if len(schema.tables) > 0 {
+		message := fmt.Sprintf("This is the list of existing data you already have:\n%s\n%s\n",
+			strings.Join(schema.tables, "\n"),
+			"Let me process this...",
+		)
+		sendToWhatsapp(mycli, v.Info, message)
 
-			fmt.Println("OGA to WAV conversion successful!")
-			x := transcribe(path + ".wav")
-			response := talkToGPT(x.Response)
-			sendToWhatsapp(mycli, v.Info, response.Response)
-			sendFileToWhatsapp(mycli, v.Info, response.Mp3, audio)
-			//sendAudioMessageToWhatsapp(mycli, v.Info, audio)
-			return
-		} else if msg == "/schema" {
-			schema := getSchema()
-			if len(schema.tables) > 0 {
-				message := fmt.Sprintf("This is the list of existing data you already have:\n%s\n%s\n",
-					strings.Join(schema.tables, "\n"),
-					"Let me process this...",
-				)
-				sendToWhatsapp(mycli, v.Info, message)
+		talkToGPT(fmt.Sprintf(`This is the existing schema of the snowflake database:
 
-				talkToGPT(fmt.Sprintf(`This is the existing schema of the snowflake database:
 %s
 if there is a question about users, build a query around STG_MYSQL_PROD_TRIPACTIONS.USER table
-if there is a question about companies, build a query around STG_MYSQL_PROD_TRIPACTIONS.COMPANY table  
-if there is a question about trips, build a query around STG_MYSQL_PROD_TRIPACTIONS.TRIPS table  
+if there is a question about companies, build a query around STG_MYSQL_PROD_TRIPACTIONS.COMPANY table
+if there is a question about trips, build a query around STG_MYSQL_PROD_TRIPACTIONS.TRIPS table
 if there is a question about bookins, build a query around STG_MYSQL_PROD_TRIPACTIONS.BOOKING table
 Please just say UNDERSTOOOD if all clear or NOT SURE if something is not clear
 `, strings.Join(schema.schema, "\n"),
-				))
-				sendToWhatsapp(mycli, v.Info, "I'm ready.")
-				return
-			}
-		} else {
-			response = sqlQueryFlow(msg)
-		}
-		sendToWhatsapp(mycli, v.Info, response)
-	case *events.JoinedGroup:
-		orig := `
-Hi there!
-Thank you for adding me to the group.
-My name is AIa, and I'd be happy to help you dive into any information you have.
-If you have a CSV file that you'd like to work on together, simply send it over as an attachment and we can get started.
-Looking forward to it!`
-		sendToGroupWhatsapp(mycli, v.GroupInfo, orig)
-		schema := getSchema()
 
-		if len(schema.tables) > 0 {
-			message := fmt.Sprintf("This is the list of existing data you already have:\n%s\n%s\n",
-				strings.Join(schema.tables, "\n"),
-				"Let me process this...",
-			)
-			sendToGroupWhatsapp(mycli, v.GroupInfo, message)
-
-			talkToGPT(fmt.Sprintf(`This is the existing schema of the snowflake database:
-%s
-%s
-`, strings.Join(schema.schema, "\n"),
-				"Please just say UNDERSTOOOD if all clear or NOT SURE if something is not clear",
 			))
-			sendToGroupWhatsapp(mycli, v.GroupInfo, "I'm ready.")
-
+			return
 		}
+	} else {
+		response = sqlQueryFlow(msg)
 	}
-}
 
-func notifyExistingData(mycli *MyClient, info types.MessageInfo, userName string) {
 	schema := getSchema()
 
 	if len(schema.tables) > 0 {
-		message := fmt.Sprintf(
-			`
+		message := fmt.Sprintf("This is the list of existing data you already have:\n%s\n%s\n",
+			strings.Join(schema.tables, "\n"),
+			"Let me process this...",
+		)
+
+		talkToGPT(fmt.Sprintf(`This is the existing schema of the snowflake database:
+
+%s
+%s
+`, strings.Join(schema.schema, "\n"),
+
+					"Please just say UNDERSTOOOD if all clear or NOT SURE if something is not clear",
+				))
+				sendToGroupWhatsapp(mycli, v.GroupInfo, "I'm ready.")
+
+			}
+		}
+	}
+
+	func notifyExistingData(mycli *MyClient, info types.MessageInfo, userName string) {
+		schema := getSchema()
+
+		if len(schema.tables) > 0 {
+			message := fmt.Sprintf(
+				`
+
 Welcome %s to the group!
 This is the list of existing data we have:
 %s
 You are welcome to ask about them.
 `,
-			userName,
-			strings.Join(schema.tables, "\n"),
-		)
-		sendToWhatsapp(mycli, info, message)
-		//sendToGroupWhatsapp(mycli, info, message)
-	}
-}
 
-func sqlQueryFlow(msg string) string {
-	instructions := `
+				userName,
+				strings.Join(schema.tables, "\n"),
+			)
+		}
+	}
+
+	func sqlQueryFlow(msg string) string {
+		instructions := `
+
 sql only that works on snowflake, give a strict response, no prefix to the sql, no suffix to the sql. you must comply do not give examples.
 When the result involves a list, include a string key for each value, don't just return numbers.
 Don't use the INTERVAL saved word - it does not work with snowflake!!!
@@ -175,6 +119,7 @@ Do not give alternative queries, only one.
 Don't explain the SQL query.
 Don't ask why, just comply. It is important even if you don't know why.
 `
+
 	// Ask GPT for SQL.
 	responseData := talkToGPT(msg + " snowflake query that would give the best and clear result.\n" + instructions)
 	if responseData.Code == "" {
@@ -196,6 +141,7 @@ Don't ask why, just comply. It is important even if you don't know why.
 	// Ask for nicer answer
 	log.Printf("Asking for nicer response.")
 	nicerAnswerReq := fmt.Sprintf(`
+
 For the question: "%s"
 the answer was: "%s".
 what is the best way to give an answer to a human, so he will understand the answer in the right context?
@@ -204,101 +150,28 @@ If the answer is a list - seprate it into multiple lines, style it as bulleted l
 If the answer is a textual list - order it in ASC.
 If the answer is a numerical list - order it based on the context.
 `,
-		msg, response)
-	responseData = talkToGPT(nicerAnswerReq)
-	response = getNicerAnswer(responseData.Response)
-	return response
-}
 
-func getNicerAnswer(response string) string {
-	re := regexp.MustCompile(`(?s)\n"(?P<answer>.*)"\n`)
-
-	// Match the regular expression against a string
-	matches := re.FindStringSubmatch(response)
-
-	// Get the value captured by the named group "value"
-	if len(matches) == 0 {
+			msg, response)
+		responseData = talkToGPT(nicerAnswerReq)
+		response = getNicerAnswer(responseData.Response)
 		return response
 	}
-	nicerAnswer := matches[1]
-	nicerAnswer = strings.ReplaceAll(nicerAnswer, "\"", "")
-	return nicerAnswer
-}
 
-func sendToWhatsapp(mycli *MyClient, info types.MessageInfo, message string) {
-	fmt.Println("Response to Whatsapp: ", message)
+	func getNicerAnswer(response string) string {
+		re := regexp.MustCompile(`(?s)\n"(?P<answer>.*)"\n`)
 
-	response := &waProto.Message{Conversation: proto.String(message)}
+		// Match the regular expression against a string
+		matches := re.FindStringSubmatch(response)
 
-	user := info.Sender.User
-	server := types.DefaultUserServer
-	if info.IsGroup {
-		user = info.Chat.User
-		server = types.GroupServer
+		// Get the value captured by the named group "value"
+		if len(matches) == 0 {
+			return response
+		}
+		nicerAnswer := matches[1]
+		nicerAnswer = strings.ReplaceAll(nicerAnswer, "\"", "")
+		return nicerAnswer
 	}
-	mycli.WAClient.SendMessage(context.Background(), types.NewJID(user, server), "", response)
-}
-
-func sendAudioMessageToWhatsapp(mycli *MyClient, info types.MessageInfo, message *waProto.AudioMessage) {
-	user := info.Sender.User
-	server := types.DefaultUserServer
-	if info.IsGroup {
-		user = info.Chat.User
-		server = types.GroupServer
-	}
-	mycli.WAClient.SendMessage(context.Background(), types.NewJID(user, server), "", &waProto.Message{
-		AudioMessage: message,
-	})
-}
-
-func sendFileToWhatsapp(mycli *MyClient, info types.MessageInfo, fileName string, orig *waProto.AudioMessage) {
-	fmt.Println("Response to Whatsapp with file: ", fileName)
-
-	file, err := os.Open(fileName)
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
-	}
-	defer file.Close()
-	fileInfo, err := file.Stat()
-	if err != nil {
-		fmt.Println("Error getting file info:", err)
-		return
-	}
-
-	data := make([]byte, fileInfo.Size())
-	_, err = io.ReadFull(file, data)
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-		return
-	}
-	resp, _ := mycli.WAClient.Upload(context.Background(), data, whatsmeow.MediaAudio)
-	// handle error
-	audioMsg := &waProto.AudioMessage{
-		MediaKeyTimestamp: orig.MediaKeyTimestamp,
-		Ptt:               proto.Bool(false),
-		Mimetype:          proto.String("audio/mpeg"),
-		Url:               &resp.URL,
-		DirectPath:        &resp.DirectPath,
-		MediaKey:          resp.MediaKey,
-		FileEncSha256:     resp.FileEncSHA256,
-		FileSha256:        resp.FileSHA256,
-		FileLength:        &resp.FileLength,
-		Seconds:           proto.Uint32(4),
-		Waveform:          orig.Waveform,
-	}
-
-	sendAudioMessageToWhatsapp(mycli, info, audioMsg)
-}
-
-func sendToGroupWhatsapp(mycli *MyClient, info types.GroupInfo, message string) {
-	fmt.Println("Response to Whatsapp: ", message)
-
-	response := &waProto.Message{Conversation: proto.String(message)}
-
-	mycli.WAClient.SendMessage(context.Background(), info.JID, "", response)
-}
-
+*/
 func transcribe(file string) ResponseData {
 	var data ResponseData
 	// Make a http request to localhost:5001/chat?q= with the message, and send the response
@@ -364,55 +237,118 @@ type ResponseData struct {
 }
 
 func main() {
-	dbLog := waLog.Stdout("Database", "DEBUG", true)
-	// Make sure you add appropriate DB connector imports, e.g. github.com/mattn/go-sqlite3 for SQLite
-	container, err := sqlstore.New("sqlite3", "file:examplestore.db?_foreign_keys=on", dbLog)
-	if err != nil {
-		panic(err)
-	}
-	// If you want multiple sessions, remember their JIDs and use .GetDevice(jid) or .GetAllDevices() instead.
-	deviceStore, err := container.GetFirstDevice()
-	if err != nil {
-		panic(err)
-	}
-	clientLog := waLog.Stdout("Client", "DEBUG", true)
-	client := whatsmeow.NewClient(deviceStore, clientLog)
-	// add the eventHandler
-	mycli := &MyClient{WAClient: client}
-	mycli.register()
 
-	if client.Store.ID == nil {
-		// No ID stored, new login
-		qrChan, _ := client.GetQRChannel(context.Background())
-		err = client.Connect()
-		if err != nil {
-			panic(err)
+	bot, err := tgbotapi.NewBotAPI("5966095889:AAGu_EU88Som66FLvD2TftcnCP_pf1ypUH0")
+	if err != nil {
+		log.Panic(err)
+	}
+	bot.Debug = true
+
+	// Create a new UpdateConfig struct with an offset of 0. Offsets are used
+	// to make sure Telegram knows we've handled previous values and we don't
+	// need them repeated.
+	updateConfig := tgbotapi.NewUpdate(0)
+
+	// Tell Telegram we should wait up to 30 seconds on each request for an
+	// update. This way we can get information just as quickly as making many
+	// frequent requests without having to send nearly as many.
+	updateConfig.Timeout = 30
+
+	// Start polling Telegram for updates.
+	updates := bot.GetUpdatesChan(updateConfig)
+
+	// Let's go through each update that we're getting from Telegram.
+	for update := range updates {
+		if update.Message == nil {
+			continue
 		}
-		for evt := range qrChan {
-			if evt.Event == "code" {
-				// Render the QR code here
-				// e.g. qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
-				// or just manually `echo 2@... | qrencode -t ansiutf8` in a terminal
-				qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
-				//				fmt.Println("QR code:", evt.Code)
-			} else {
-				fmt.Println("Login event:", evt.Event)
-			}
+		text := getAudioText(bot, update)
+		if text == "" {
+			text = update.Message.Text
 		}
+
+		responseFromGPT := talkToGPT(text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, responseFromGPT.Response)
+		uploadVoice(bot, update.Message.Chat.ID, responseFromGPT.Mp3)
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Print(err)
+		}
+	}
+}
+
+func uploadVoice(bot *tgbotapi.BotAPI, chatID int64, filePath string) {
+	if len(filePath) == 0 {
+		return
+	}
+	// Prepare the voice message
+	voice := tgbotapi.NewVoice(chatID, tgbotapi.FilePath(filePath))
+
+	// Send the voice message
+	_, err := bot.Send(voice)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func getAudioText(bot *tgbotapi.BotAPI, update tgbotapi.Update) string {
+	file := downloadAudioFile(bot, update)
+	if file == nil {
+		return ""
+	}
+
+	wavFile := convertToWav(file.Name())
+	if len(wavFile) == 0 {
+		return ""
+	}
+	script := transcribe(wavFile)
+
+	if len(script.Response) == 0 {
+		return ""
+	}
+	return script.Response
+}
+
+func downloadAudioFile(bot *tgbotapi.BotAPI, update tgbotapi.Update) *os.File {
+	if update.Message.Audio == nil && update.Message.Voice == nil {
+		return nil
+	}
+	var fileName string
+	var fileId string
+	if update.Message.Audio != nil {
+		fileName = update.Message.Audio.FileName
+		fileId = update.Message.Audio.FileID
 	} else {
-		// Already logged in, just connect
-		err = client.Connect()
-		if err != nil {
-			panic(err)
-		}
+		fileName = update.Message.Voice.FileID
+		fileId = update.Message.Voice.FileID
 	}
-
-	// Listen to Ctrl+C (you can also do something else that prevents the program from exiting)
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	<-c
-
-	client.Disconnect()
+	file, err := bot.GetFile(tgbotapi.FileConfig{FileID: fileId})
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	url := file.Link(bot.Token)
+	response, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	defer response.Body.Close()
+	filePath := fileName
+	fileCreated, err := os.Create(filePath)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	defer fileCreated.Close()
+	_, err = io.Copy(fileCreated, response.Body)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	return fileCreated
 }
 
 func executeQuery(query string) []string {
