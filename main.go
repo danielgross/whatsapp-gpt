3d5c8136242cad2d4f7fee273d8c6daa5254f95e
Shushu Inbar <iinbar@tripactions.com>
2023-01-22 01:07:08 +0200

    support snowflake

--- main.go
+++ main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/PullRequestInc/go-gpt3"
 	_ "github.com/go-sql-driver/mysql"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/snowflakedb/gosnowflake"
 	"io"
 	"log"
+	"math"
 	"net/http"
 	"net/url"
 	"os"
 	"os/exec"
 	"strings"
 )
+
+var conversation Conversation
+
+type Conversation struct {
+	fullMessage  string
+	lastRequest  string
+	lastResponse string
+	schema       string
+}
 
 func getConfig() gosnowflake.Config {
 	return gosnowflake.Config{
@@ -201,6 +213,35 @@ func transcribe(file string) ResponseData {
 	return data
 }
 
+func record(message string) ResponseData {
+	var data ResponseData
+	// Make a http request to localhost:5001/chat?q= with the message, and send the response
+	// URL encode the message
+	urlEncoded := url.QueryEscape(message)
+	url := "http://localhost:5001/record?q=" + urlEncoded
+	// Make the request
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return data
+	}
+	defer resp.Body.Close()
+
+	decoder := json.NewDecoder(resp.Body)
+	for {
+		// Decode the next value in the response
+		err = decoder.Decode(&data)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			// Handle the error
+		}
+	}
+
+	return data
+}
+
 func talkToGPT(message string) ResponseData {
 	var data ResponseData
 	// Make a http request to localhost:5001/chat?q= with the message, and send the response
@@ -266,11 +307,23 @@ func main() {
 		if text == "" {
 			text = update.Message.Text
 		}
+		conversation.fullMessage += "\n" + text
 
-		responseFromGPT := talkToGPT(text)
+		sqlQuery := promptSqlExpert(conversation.fullMessage)
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, responseFromGPT.Response)
-		uploadVoice(bot, update.Message.Chat.ID, responseFromGPT.Mp3)
+		sqlQuery = strings.ReplaceAll(sqlQuery, "tbooking", "dwh.mart_reporting.mart_booking")
+		sqlQuery = strings.ReplaceAll(sqlQuery, "tcompany", "DWH.MART_REPORTING.MART_COMPANY")
+		sqlResult := strings.Join(executeQuery(sqlQuery), "\n")
+		answer := sqlResult
+
+		conversationExpertTextRaw := explainSqlQueryExpert(sqlQuery)
+
+		lastAnswer := conversationExpertTextRaw + "\n" + sqlResult
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, lastAnswer)
+		toMp3 := explainQuestionAndAnswer(conversation.fullMessage, answer)
+		mp3 := record(toMp3)
+		uploadVoice(bot, update.Message.Chat.ID, mp3.Mp3)
 
 		if _, err := bot.Send(msg); err != nil {
 			log.Print(err)
@@ -379,9 +432,9 @@ func executeQuery(query string) []string {
 	var result []string
 
 	cols, err := rows.Columns()
-	//header := strings.Join(cols, ",")
-	//result = append(result, header)
-	//result = append(result, strings.Repeat("-", len(header)))
+	header := strings.Join(cols, ",")
+	result = append(result, header)
+	result = append(result, strings.Repeat("-", len(header)))
 
 	pointers := make([]interface{}, len(cols))
 	container := make([]string, len(cols))
@@ -468,4 +521,84 @@ func getSchema() ExistingSchema {
 		return schema
 	}
 	return schema
+}
+
+func buildPrompt(question string) string {
+	promptTables := `
+	You are a snowflake DBA.  Follow these guidelines:
+	1. Always use ilike when comparing text.  user name, CSM manager, company name...
+	2. Never use email unless the field name contains 'EMAIL'
+	3. When asked about hotel, flight, car bookings for a specific user always user BOOKING_USER
+	4. When asked about Navan, add IS_REED_MACKAY_BOOKING='FALSE'
+	5. Always remove duplicates.
+	6. When counting, always use DISTINCT on what you are about to count
+	7. When you are asked to add something, you need to add it to the select statement
+	8. When dividing, always use NULLIFZERO
+	9. Always end an SQL SELECT query with a ; char
+	INPUT: Ilan Twig's bookings
+	OUTPUT: BOOKING_USER ilike '%Ilan Twig%'
+		###Snowflake tables,with their properties:
+		#
+		#table tbooking, columns=[COMPANY_UUID use DISTINCT for counting,AGENCY_UUID,AGENCY_PCC,AGENCY_NAME,REPORT_DATETIME use this for booking date,REPORT_DATETIME_PST use this for booking date in pacific timezone,ACTIVE_BOOKING,BAGGAGE_FEE,BASE_PRICE use this for total calculating travel spend.  it is in USD,BASE_PRICE_LOCAL,BOOKING_CURRENCY,BOOKING_ID,BOOKING_IMPERSONATOR_PERSON_UUID,BOOKING_STATUS,CAR_VEHICLE_TYPE,CAR_DROPOFF_DATETIME,CHECK_IN_DATETIME,CHECK_OUT_DATETIME,CONFIRMATION_NUMBER,CREDIT_CARD_LAST4_DIGITS,CREDIT_CARD_TYPE,DATE_CANCELLED,DATE_MODIFIED,DESTINATION_CITY,DESTINATION_COUNTRY,DESTINATION_STATE,DIM_HOTEL_CITY,END_DATE,ESTIMATED_SPEND,EXCHANGE_FEE,FAIR_PRICE,FEE_TYPE,FLIGHT_OUTBOUND_ARRIVAL_DATETIME,FLIGHT_CABIN_CLASS use this for flight class,FLIGHT_CARRIER_LOCATOR_LIST,FLIGHT_CARRIER,FLIGHT_OUTBOUND_DEPARTURE_DATETIME,FLIGHT_MAIN_FARE_CLASS,FLIGHT_MAJOR_AIRLINE_CODE,FLIGHT_NUMBER_LIST,GDS_FEES,GROSS_SAVING_PERCENTAGE,HAS_HOTEL_BREAKFAST_INCLUDED,HAS_VALID_START_DATE,HOTEL_CORPORATE_DISCOUNT,HOTEL_INVENTORY_PROVIDER,HOTEL_NAME,HOTEL_PARENT_CHAIN_NAME,EXPEDIA_HOTEL_CHAIN,EXPEDIA_PARENT_CHAIN_NAME,HOTEL_PAYMENT_SCHEDULE,HOTEL_SABRE_CHAIN_CODE,HOTEL_SABRE_RATE_ACCESS_CODE,HOTEL_STARS_RATING,HOTEL_UUID,HOTEL_ROOM_GROUP_NAME,IS_AFFILIATE_PURCHASE_WITH_POINTS,IS_AGENT_MADE_BOOKING,IS_BOOKED_FOR_SELF,IS_BOOKING_WITH_REWARD,IS_BOOKING_WITH_SAVING,IS_OUT_OF_POLICY,IS_PERSONAL,IS_REFUNDABLE,IS_REED_MACKAY_BOOKING,IS_REED_MACKAY_ME_BOOKING,IS_REED_MACKAY_VIP_BOOKING,IS_TRANSACTED_ON_TRIPACTIONS,IS_TRIPACTIONS_LODGING_COLLECTION_RATE,IS_NEGOTIATED_DISCOUNT,NEGOTIATED_DISCOUNT_TYPE,LONG_HAUL_DURATION_THRESHOLD,LONGEST_FLIGHT_DURATION,NET_SAVING_PERCENTAGE,NET_SAVING,ORIGIN_CITY,ORIGIN_COUNTRY,ORIGIN_STATE,POLICY_VIOLATION_REASON,PRICE_BEFORE_FEE_LOCAL,PRICE_BEFORE_FEE,PRICE_TO_BEAT,PROVIDER,RESORT_FEE,REVENUE_PROVIDER,REVENUE,REWARD_VALUE,SAVING,SEATS_FEES_BASE,SEATS_FEES_TAX_LOCAL,SEATS_FEES_TAX,SOURCE_CURRENCY_CONVERSION,SOURCE_CURRENCY,START_DATE,TAX_LOCAL,TAX,TOTAL_PRICE_LOCAL,TOTAL_PRICE,TRAVEL_DURATION,TRIP_FEE_LOCAL,TRIP_FEE,TRIP_PURPOSE_GROUP,TRIP_ROUTE_TYPE,VALID_BOOKING,VENDOR,VIP_FEE,BOOKING_TRIP_PURPOSE,BOOKING_TRIP_NAME,BOOKING_TYPE can be flight/hotel/car/black car. always use ilike with this field,FRONT_END_REVENUE,ESTIMATE_PERCENTAGE_YIELD_OF_BOOKING_VALUE,ESTIMATED_TOTAL_BOOKING_REVENUE,TOTAL_ROOM_NIGHTS,LEAD_TIME_DAYS,IS_TRIP_INVITE,IS_TEAM_EVENT_INVITE indicates that this was a team event booking,TIME_TO_BOOK_MINUTES,DEPARTMENT,BOOKING_USER_EMAIL,BOOKING_USER always use ilike with this field.  Use it for questions about booking by user name,BOOKING_USER_GENDER,IS_ADMIN use to know if the user or booker is admin,IS_BUYER,IS_COMPANY_DELEGATE,BOOKING_USER_IS_EXECUTIVE_ASSISTANT,IS_INFLUENCER,IS_PROGRAM_MANAGER,IS_ROAD_WARRIOR,IS_SUPER_ADMIN,IS_VIP,POLICY_LEVEL,REGION,SUBSIDIARY,BOOKING_USER_TITLE,TRAVEL_FREQUENCY,BOOKING_USER_MANAGER,booking_user_is_executive_assistant,TRAVELLER_LEGAL_ENTITY_NAMES,TRAVELLER_LEGAL_ENTITY_UUIDS,TRAVELLER_MANAGER_EMAILS,TRAVELLER_OFFICE_COUNTRIES,TRAVELLER_OFFICE_LEGAL_NAMES,TRAVELLER_USERS_EMAILS,TRAVELLER_USERS,ACCOUNT_EXECUTIVE_MANAGER,ACCOUNT_EXECUTIVE,ACCOUNT_ID,ACCOUNT_OWNER,ACCOUNT_OWNER_EMAIL,ACCOUNT_STATUS,COMPANY_NAME always use ilike with this field,COMPANY_STATUS,GLOBAL_CSM_DIRECTOR,GLOBAL_CSM_DIRECTOR_EMAIL,GLOBAL_CSM_DIVISION,GLOBAL_CSM_MANAGER,GLOBAL_CSM_MANAGER_EMAIL,GLOBAL_CSM,GLOBAL_CSM_EMAIL,INDUSTRY,IS_REWARDS_ENABLED,MARKET_SEGMENT,PREVIOUS_TRAVEL_MGMT,REFERRER,SALESFORCE_ACCOUNT_TYPE,AFFILIATE_PRICING_MODEL,DOMAIN,IS_AFFILATE_PROGRAM_ENABLED,IS_COMPANY_LHG_SELF_ONBOARDING,LAUNCH_DATE,NUM_EMPLOYEES,REGIONAL_CSM_UUID,REGIONAL_CSM,REGIONAL_CSM_EMAIL,TRIPACTIONS_REGION,AGENCY_COUNTRY_NAME always use ilike,SABRE_FLIGHT_EXTRAS_TOTAL_PRICE,SABRE_NUMBER_OF_FLIGHT_CHANGES,ORIGIN_CONTINENT,ORIGIN_REGION,DESTINATION_CONTINENT,DESTINATION_REGION,POLICY_TYPE,MAX_ROOM_PRICE_PER_NIGHT_ALLOWED,MAX_PRICE_ALLOWED,COMPANY_POLICY_TYPE,COMPANY_BILLING_COUNTRY_CODE,COMPANY_OPPORTUNITY_CLOSE_DATE,IS_RM_ACCOUNT,COMPANY_BILLING_COUNTRY_NAME,IS_LIQUID,IS_VIP_BOOKING,REGIONAL_CSM_MANAGER,GLOBAL_CSM_MANAGER,flight_booking_distance_miles,airline_route];
+		#
+		### `
+
+	return promptTables + question + "\nSELECT"
+}
+
+func promptSqlExpert(question string) string {
+	fmt.Println("Full questions:\n", question)
+	prompt := buildPrompt(question)
+	ctx := context.Background()
+	client := gpt3.NewClient(os.Getenv("API_KEY"))
+	response, err := client.CompletionWithEngine(ctx, "code-davinci-002", gpt3.CompletionRequest{
+		Prompt:      []string{prompt},
+		Temperature: gpt3.Float32Ptr(0),
+		MaxTokens:   gpt3.IntPtr(300),
+		Stop:        []string{"#", ";"},
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return "SELECT " + response.Choices[0].Text
+}
+
+func explainSqlQueryExpert(whatToExplain string) string {
+	trimmed := whatToExplain[:int(math.Min(float64(len(whatToExplain)), float64(300)))]
+	prompt := "You are a high school teacher.  Explain what the blow query means:" +
+		" in the most simple way.\nQuery:" + trimmed + "\n"
+	return explain(prompt)
+}
+
+func explainQuestionAndAnswer(question string, answer string) string {
+	prompt := fmt.Sprintf(`
+You are a high school teacher.
+User asked the question: "%s"
+The answer is: "%s"
+Please give a simple explanation: 
+`, question, answer)
+	return explain(prompt)
+}
+
+func explain(prompt string) string {
+	fmt.Println("Prompt to GPT3: ", prompt)
+	ctx := context.Background()
+	client := gpt3.NewClient(os.Getenv("API_KEY"))
+	response, err := client.CompletionWithEngine(ctx, "text-davinci-003", gpt3.CompletionRequest{
+		Prompt:           []string{prompt},
+		Temperature:      gpt3.Float32Ptr(0.5),
+		TopP:             gpt3.Float32Ptr(1.0),
+		FrequencyPenalty: 0.0,
+		PresencePenalty:  0.0,
+		MaxTokens:        gpt3.IntPtr(300),
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Answer from GPT3: ", response.Choices[0].Text)
+
+	return response.Choices[0].Text
 }
