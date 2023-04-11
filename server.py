d2f20b5c518cf28704ab402b29cad62a687d0f34
Huskynarr <admin@huskynarr.de>
2023-04-11 23:53:58 +0200

    Update server.py

--- server.py
+++ server.py
@@ -13,9 +13,10 @@ PROFILE_DIR = "/tmp/playwright" if '--profile' not in sys.argv else sys.argv[sys
 PORT = 5001 if '--port' not in sys.argv else int(sys.argv[sys.argv.index('--port') + 1])
 APP = flask.Flask(__name__)
 PLAY = sync_playwright().start()
-BROWSER = PLAY.chromium.launch_persistent_context(
+BROWSER = PLAY.firefox.launch_persistent_context(
     user_data_dir=PROFILE_DIR,
-    headless=False,
+    headless=True,
+    java_script_enabled=True,
 )
 PAGE = BROWSER.new_page()
 
